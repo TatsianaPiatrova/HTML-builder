6962322f04ce00427fd2a92008f77b00d3be288d
Tatsiana <tania.petrova@mail.ru>
2022-05-23 20:30:21 +0300

    feat: task 6 is done

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -10,8 +10,9 @@ const stylePath = path.join(__dirname, 'styles');
 const styleFile = 'style.css';
 const newStylePath = path.join(projectPath, styleFile);
 
-// const index = 'index.html';
-// const template = path.join(__dirname, 'template.html');
+const index = 'index.html';
+const indexPath = path.join(__dirname, project, index);
+const template = path.join(__dirname, 'template.html');
 
 
 async function createDir(){
@@ -54,43 +55,45 @@ async function copyAssets(){
   await copyDir(path.join(__dirname, 'assets'), path.join(outputDirPath, 'assets'));
 }
 
-// function addContentToFile(path, data) { 
-//   fs.appendFile(path, data, (err) => {if (err) throw err;});
-// }
+function addContentToFile(path, data) { 
+  fs.appendFile(path, data, (err) => {if (err) throw err;});
+}
 
-// //in this self-calling function created html (string) from template and tags
-// (async function () {
-//   //content from Template into strTemplate (string)    
-//   const templateFile = async () => {
-//     const templateContent = await fsPromise.readFile(pathTemplate, 'utf-8', (err) => { if (err) throw err });
-//     return await templateContent.toString();
-//   };
-//   let strTemplate = await templateFile(); //content from template.html
-//   //tag from strTemplate into array - tags
-//   const tags = strTemplate.match(/{{[a-z]+}}/gi);  //example: ['{{header}}',...]
-//   //circle on array - function for replace template tags contents from files 
-//   async function processArray(array) {
-//     for (const tag of array) {
-//       let tag1 = tag.substring(2, tag.length - 2); //example: 'header'
-//       let fileName = tag1 + '.html';
-//       let pathTag = path.join(__dirname, 'components', `${fileName}`);
-//       const tagFile = async () => {  //function - read content(tagContent) from file(pathTag)
-//         const tagContent = await fsPromise.readFile(pathTag, 'utf-8', (err) => { if (err) throw err; });
-//         return await tagContent.toString();
-//       };
-//       let strTag = await tagFile();
-//       strTemplate = strTemplate.replace(tag, strTag);
-//     }
-//     return await strTemplate;
-//   }
-//   const newHtml = await processArray(tags); //string newHtml - call function 
-//   addContentToFile(pathFileToHtml, newHtml);    //call function for write content(newHtml) into file 
-// })();
+async function createHTML() {
+ 
+  const templateFile = async () => {
+    const templateContent = await fs.promises.readFile(template, 'utf-8', (err) => { if (err) throw err; });
+    return await templateContent.toString();
+  };
+
+  let strTemplate = await templateFile(); 
+
+  const tags = strTemplate.match(/{{[a-z]+}}/gi);  
+
+  async function processArray(array) {
+    for (const tag of array) {
+      let tag1 = tag.substring(2, tag.length - 2); 
+      let fileName = tag1 + '.html';
+      let pathTag = path.join(__dirname, 'components', `${fileName}`);
+      const tagFile = async () => {  
+        const tagContent = await fs.promises.readFile(pathTag, 'utf-8', (err) => { if (err) throw err; });
+        return await tagContent.toString();
+      };
+      let strTag = await tagFile();
+      strTemplate = strTemplate.replace(tag, strTag);
+    }
+    return await strTemplate;
+  }
+
+  const newHtml = await processArray(tags); 
+  addContentToFile(indexPath, newHtml);    
+}
 
 async function create(){
   await createDir();  
   copyAssets();
   createStyle();
+  createHTML();
 }
 
 create();
